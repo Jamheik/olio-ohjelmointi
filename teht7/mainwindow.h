cf17f0f598963793593eb2c289dee4ba3462fa35
Jamheik <heikkinenjami03@gmail.com>
2024-02-25 17:53:22 +0200

    teht 7

---
+++ teht7/mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#include <QProgressBar>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    short player1Time = 0;
+    short player2Time = 0;
+    short gameTime;
+    short currentPlayer;
+    QTimer *gameTimer;
+    void updateProgressBar();
+    void updateDisplayText(QString);
+    void resetTime();
+
+public slots:
+    void timeout();
+
+private slots:
+    void on_btn2min_clicked();
+    void on_btn5min_clicked();
+    void on_btnStartGame_clicked();
+    void on_btnStopGame_clicked();
+    void on_btnSwitchP1_clicked();
+    void on_btnSwitchP2_clicked();
+
+    void on_btnSwitcP1_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
